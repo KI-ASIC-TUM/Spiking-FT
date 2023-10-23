fabd5c5c3fbf30de2cac2b3baf665af69c49ad32
jlrandulfe <lopez.randulfe@tum.de>
2023-10-23 17:14:28 +0200

    Add routine for generating single frame plot

--- examples/generate_video.py
+++ examples/generate_video.py
@@ -34,6 +34,20 @@ class Animator():
         self.im.set_data(self.images[i])
         self.line.set_ydata(self.sequence[i][2:])
 
+def gen_frame(in_data, in_image):
+        fig, ax = plt.subplots(2)
+        im = ax[0].imshow(in_image)
+        ax[0].set_title("Camera input", fontsize=16)
+        ax[0].set_xticks([])
+        ax[0].set_yticks([])
+        line, = ax[1].plot(in_data[2:], linewidth=2)
+        ax[1].set_yticks([])
+        ax[1].set_title("Spiking FT", fontsize=16)
+        ax[1].set_xlabel("Bin Nº", fontsize=13)
+        ax[1].spines['left'].set_visible(False)
+        ax[1].spines['top'].set_visible(False)
+        ax[1].spines['right'].set_visible(False)
+        plt.savefig("spinn_out_10.pdf", dpi=150)
 
 def get_images():
     handler = dhandler.oth_handler.OTHHandler("OTH/20230127_114455")
@@ -61,6 +75,7 @@ def main(n_frames=111):
     out_sequence = collect_data(n_frames)
     images = get_images()
     gen_video(out_sequence, images, n_frames)
+    gen_frame(out_sequence[10], images[10])
 
 
 if __name__ == "__main__":
