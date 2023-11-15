26613e2995c5b2756cdbf9729abeb58d7c04a3c8
jlrandulfe <jlrandulfe@gmail.com>
2023-11-15 17:06:48 +0100

    Add plot of CFAR

--- examples/01e-cfar_experiment_numpy.py
+++ examples/01e-cfar_experiment_numpy.py
@@ -21,13 +21,35 @@ sim_times = [
     72, 80, 88, 96, 104, 112, 120,
     128, 136, 144, 152, 160, 168,
     176, 184, 192, 200, 208, 216,
-    224, 232, 240, 248, 256, 257
+    224, 232, 240, 248, 256, 268,
+    274, 282, 290, 300, 325, 350,
+    375, 400, 425, 450, 475, 500
 ]
+# sim_times = [200]
 
-bin_sizes = [64, 256, 1024]
+bin_sizes = [1024]
+
+# One width per bin size
+cfar_widths = [16, 16, 16]
 
 
-def run(raw_data, timesteps, out_type, title):
+def plot_cfar(ft, std_targets, sft, spinn_targets, n_bins, n_steps, scene_n):
+    plt.subplot(2, 1 ,1)
+    plt.plot(ft)
+    for target in std_targets:
+        plt.axvline(x=target, color="orange", linestyle="--")
+    plt.title("FFT {} bins".format(n_bins))
+    plt.subplot(2, 1 ,2)
+    plt.plot(sft)
+    for target in spinn_targets:
+        plt.axvline(x=target, color="orange", linestyle="--")
+    plt.title("S-FT {} steps".format(n_steps))
+    plt.tight_layout()
+    plt.savefig("results/cfar_{}bins_scenario{}".format(n_bins, scene_n))
+    plt.show()
+
+
+def run(raw_data, timesteps, out_type, title, cfar_width, scene_n):
     fft_shape = raw_data.shape
     pre_pipeline = sft.preproc_pipeline.PreprocPipeline(fft_shape)
 
@@ -37,6 +59,7 @@ def run(raw_data, timesteps, out_type, title):
         "out_format": "modulus",
         "normalize": True,
         "off_bins": 4,
+        "unitary": True
     }
 
     encoder_params = {
@@ -55,51 +78,81 @@ def run(raw_data, timesteps, out_type, title):
     }
     oscfar_params = {
         "n_dims": 1,
-        "window_width": 16,
+        "window_width": 32,
         "ordered_k": 6,
-        "alpha": 0.2,
-        "n_guard_cells": 2,
+        "alpha": 0.4,
+        "n_guard_cells": 4,
+    }
+    spinn_oscfar_params = {
+        "n_dims": 1,
+        "window_width": 32,
+        "ordered_k": 6,
+        "alpha": 0.4,
+        "n_guard_cells": 4,
     }
     encoder = sft.encoder.Encoder(fft_shape, **encoder_params)
     s_dft = sft.s_dft_numpy.SDFT(encoder.out_data_shape, **sft_params)
-    oscfar = pyrads.algms.os_cfar.OSCFAR(
+    spinn_oscfar = pyrads.algms.os_cfar.OSCFAR(
         s_dft.out_data_shape,
-        **oscfar_params
+        **spinn_oscfar_params
     )
 
-    spinn_pipeline = pyrads.pipeline.Pipeline([pre_pipeline, encoder, s_dft, oscfar])
+    spinn_pipeline = pyrads.pipeline.Pipeline([pre_pipeline, encoder, s_dft, spinn_oscfar])
     spinn_pipeline(raw_data)
-    s_dft_out = spinn_pipeline.output[-1]
+    s_dft_out = spinn_pipeline.output[-2]
+    spinn_cfar = spinn_pipeline.output[-1]
+    spinn_targets = np.where(spinn_cfar)[-1]
     np.save("results/snn_{}.npy".format(title), s_dft_out)
 
     fft_alg = pyrads.algms.fft.FFT(
         fft_shape,
         **fft_params
     )
+    oscfar = pyrads.algms.os_cfar.OSCFAR(
+        s_dft.out_data_shape,
+        **oscfar_params
+    )
     std_pipeline = pyrads.pipeline.Pipeline([pre_pipeline, fft_alg, oscfar])
     std_pipeline(raw_data)
-    fft_out = std_pipeline.output[-1]
+    fft_out = std_pipeline.output[-2]
+    std_cfar = std_pipeline.output[-1]
+    targets = np.where(std_cfar)[-1]
+    # plot_cfar(
+    #     fft_out[0,0,0,0,:],
+    #     targets,
+    #     s_dft_out[0,0,0,0,:],
+    #     spinn_targets,
+    #     raw_data.size,
+    #     timesteps,
+    #     scene_n
+    # )
     np.save("results/std_{}.npy".format(title), fft_out)
-    return (s_dft_out, fft_out)
+    return (spinn_cfar, std_cfar)
 
 
-def run_batch(raw_data, timesteps):
-    acc_avg = 0
+def run_batch(raw_data, timesteps, cfar_width):
+    acc_avg, prec_avg, rec_avg = (0, 0, 0)
     for i in range(4):
         title = "TI_{}".format(i)
-        s_dft_out, fft_out = run(raw_data[i], timesteps, "spike", title)
+        s_dft_out, fft_out = run(raw_data[i], timesteps, "spike", title, cfar_width, i)
         # Measure accuracy
         acc = sft.utils.metrics.get_accuracy(s_dft_out, fft_out)
+        prec = sft.utils.metrics.get_accuracy(s_dft_out, fft_out)
+        rec = sft.utils.metrics.get_accuracy(s_dft_out, fft_out)
         acc_avg += acc
+        prec_avg += prec
+        rec_avg += rec
         # print(rmse)
         # Plot results
         # fft_data = fft_out[0, 0, 0, 0, :]
         # fig, axs = plt.subplots(2, figsize=(10,6))
     acc_avg /= 4
-    return acc_avg
+    prec_avg /= 4
+    rec_avg /= 4
+    return (acc_avg, prec_avg, rec_avg)
 
 
-def run_experiment(raw_data, n_bins):
+def run_experiment(raw_data, n_bins, cfar_width):
     # Reduce data size based on number of bins
     if n_bins == 64:
         step = 16
@@ -110,10 +163,16 @@ def run_experiment(raw_data, n_bins):
     raw_data = raw_data[:,:,:,:,:,::step]
     # Iterate for different simulation times
     acc = []
+    prec = []
+    rec = []
     for i in range(len(sim_times)):
-        acc_avg = run_batch(raw_data, timesteps=sim_times[i])
+        acc_avg, prec_avg, rec_avg = run_batch(raw_data, sim_times[i], cfar_width)
         acc.append(acc_avg)
-    plt.plot(sim_times, acc, label=r'$n_{{bins}}={}$'.format(n_bins))
+        prec.append(prec_avg)
+        rec.append(rec_avg)
+    plt.plot(sim_times, acc, label="Accuracy")
+    plt.plot(sim_times, prec, label="Precision")
+    plt.plot(sim_times, rec, label="Recall")
     return
 
 
@@ -130,8 +189,8 @@ def main(
         # reshape to standard data format
         raw_data = raw_data.reshape((4, 1,1,1,1, 1024))
 
-    for n_bins in bin_sizes:
-        run_experiment(raw_data, n_bins)
+    for i, n_bins in enumerate(bin_sizes):
+        run_experiment(raw_data, n_bins, cfar_widths[i])
     plt.legend()
     plt.savefig("results/TI_rmse.eps")
     plt.show()
