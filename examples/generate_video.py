f9eb4a86a0a16f4de6a619cd995f826181784147
jlrandulfe <jlrandulfe@gmail.com>
2024-11-17 13:22:21 +0100

    Minor change to generate_video script

--- examples/generate_video.py
+++ examples/generate_video.py
@@ -35,19 +35,20 @@ class Animator():
         self.line.set_ydata(self.sequence[i][2:])
 
 def gen_frame(in_data, in_image):
-        fig, ax = plt.subplots(2)
-        im = ax[0].imshow(in_image)
-        ax[0].set_title("Camera input", fontsize=16)
-        ax[0].set_xticks([])
-        ax[0].set_yticks([])
-        line, = ax[1].plot(in_data[2:], linewidth=2)
-        ax[1].set_yticks([])
-        ax[1].set_title("Spiking FT", fontsize=16)
-        ax[1].set_xlabel("Bin Nº", fontsize=13)
-        ax[1].spines['left'].set_visible(False)
-        ax[1].spines['top'].set_visible(False)
-        ax[1].spines['right'].set_visible(False)
-        plt.savefig("spinn_out_10.pdf", dpi=150)
+    fig, ax = plt.subplots(2)
+    im = ax[0].imshow(in_image)
+    ax[0].set_title("Camera input", fontsize=16)
+    ax[0].set_xticks([])
+    ax[0].set_yticks([])
+    line, = ax[1].plot(in_data[2:], linewidth=2)
+    ax[1].set_yticks([])
+    ax[1].set_title("Spiking FT", fontsize=16)
+    ax[1].set_xlabel("Bin Nº", fontsize=13)
+    ax[1].spines['left'].set_visible(False)
+    ax[1].spines['top'].set_visible(False)
+    ax[1].spines['right'].set_visible(False)
+    plt.savefig("spinn_out_42.pdf", dpi=142)
+    plt.show()
 
 def get_images():
     handler = dhandler.oth_handler.OTHHandler("OTH/20230127_114455")
@@ -74,8 +75,8 @@ def main(n_frames=111):
     """
     out_sequence = collect_data(n_frames)
     images = get_images()
-    gen_video(out_sequence, images, n_frames)
-    gen_frame(out_sequence[10], images[10])
+    # gen_video(out_sequence, images, n_frames)
+    gen_frame(out_sequence[42], images[42])
 
 
 if __name__ == "__main__":
